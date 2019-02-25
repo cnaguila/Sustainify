86de7c050853d1c21dffb50d7a332a686d166ed3
cnaguila <38264063+cnaguila@users.noreply.github.com>
2019-02-25 13:35:22 -0800

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import HomeScreen from '../screens/HomeScreen';
+import OptionScreen from '../screens/OptionScreen';
+import DisplayImage from '../screens/DisplayImage';
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
+  Home: HomeScreen,
+  Option: OptionScreen,
+  Display: DisplayImage,
 }));
