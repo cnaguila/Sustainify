c92dab5e38293ccf76dc368c139be5413a2ec9ba
amnunez1 <amnunez1@uci.edu>
2019-03-12 13:25:44 -0700

    Add files via upload

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import HomeScreen from '../screens/HomeScreen';
+import OptionScreen from '../screens/OptionScreen';
+import DisplayImage from '../screens/DisplayImage';
+import ClosetScreen from '../screens/ClosetScreen';
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
+  Home: HomeScreen,
+  Option: OptionScreen,
+  Display: DisplayImage,
+  Closet: ClosetScreen
 }));
